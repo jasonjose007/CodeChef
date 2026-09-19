c907274cf20bde7fb91b685e88585dd1a219a33b
Jason <jasonjose006@gmail.com>
2026-09-19 16:15:08 +0530

    CC-START01 [Runtime: 0.01; Memory: N/A]

--- START01.py
+++ START01.py
@@ -1,13 +1,7 @@
 # Number Mirror
 # Platform: CodeChef
 # Difficulty: Easy
+# Topics: Algorithms
 
-import sys
-
-def main():
-    input_data = sys.stdin.read().split()
-    if input_data:
-        print(input_data[0])
-
-if __name__ == '__main__':
-    main()
+n = input()
+print(n)
