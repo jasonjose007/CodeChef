b0448564d93533ded2d6665442f5ca0994ce4214
Jason <jasonjose006@gmail.com>
2026-09-23 14:58:54 +0530

    CC-FLOW004 [Runtime: 0.01; Memory: N/A]

--- FLOW004.py
+++ FLOW004.py
@@ -1,6 +1,7 @@
 # First and Last Digit
 # Platform: CodeChef
 # Difficulty: Easy
+# Topics: Mathematics, Algorithms
 
 import sys
 
@@ -8,14 +9,13 @@ def solve():
     input_data = sys.stdin.read().split()
     if not input_data:
         return
+    
     t = int(input_data[0])
-    results = []
     for i in range(1, t + 1):
         s = input_data[i]
-        first = int(s[0])
-        last = int(s[-1])
-        results.append(str(first + last))
-    print('\n'.join(results))
+        first_digit = int(s[0])
+        last_digit = int(s[-1])
+        print(first_digit + last_digit)
 
 if __name__ == '__main__':
     solve()
